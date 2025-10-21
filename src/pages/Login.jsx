ff810d56bfacb41f231775325717b9cc50bcb53f
1197chandu <jadhavc1197@gmail.com>
2025-10-21 19:24:35 +0530

    Implemented dummy admin login with Tailwind styling

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,37 @@
-import React from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
-  return <div>Login</div>;
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleLogin = () => {
+    if (password === "admin123") {
+      localStorage.setItem("isAdmin", true);
+      navigate("/admin");
+    } else {
+      alert("Invalid password!");
+    }
+  };
+
+  return (
+    <div className="max-w-sm mx-auto mt-20 bg-white p-6 rounded-lg shadow-md w-[50%] flex flex-col justify-center items-center">
+      <h2 className="text-2xl font-bold mb-4 text-center">Admin Login</h2>
+      <input
+        type="password"
+        placeholder="Enter admin password"
+        className=" border rounded px-3 py-2 mb-4"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button
+        onClick={handleLogin}
+        className="bg-blue-500 text-white py-2 rounded hover:bg-blue-600 w-[10%]"
+      >
+        Login
+      </button>
+    </div>
+  );
 };
 
 export default Login;
